d23e7ff1e7bc020e22bf1c3ba53ee93cf6cfd3eb
Danton <dantyt337@gmail.com>
2024-08-08 17:43:32 -0300

    Atualizando App.css e App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [champInfo, setChampInfo] = useState([]);
+
+  useEffect(() => {
+    let url = 'https://ddragon.leagueoflegends.com/cdn/12.10.1/data/en_US/champion.json';
+    fetch(url)
+      .then((r) => r.json()) 
+      .then((json) => {
+        setChampInfo(Object.values(json.data));
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/DrMundo_0.jpg" alt="Fundo Header" id="FundoHeader"></img>
+        <img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/9eb028de391e65072d06e77f06d0955f66b9fa2c-736x316.png?auto=format&fit=fill&q=80&w=625" id="LogoLol" alt="Logo LoL"></img>
       </header>
+
+      <hr/>
+
+      <div id="Info">
+        {champInfo.map((champ) => (
+          <div id="ChampCard" key={champ.id}>
+            <img
+              src={`https://ddragon.leagueoflegends.com/cdn/12.10.1/img/champion/${champ.image.full}`} 
+              alt={`Imagem do campeão ${champ.name}`}/>
+            <strong id="champ-name">{champ.name}</strong>
+          </div>
+        ))}
+      </div>
+
+      <footer>
+        <img src="https://i.scdn.co/image/ab6761610000e5ebe80d1ffb81aa6503ad41c574" alt="Logo League of Legends"></img>
+      </footer>
     </div>
   );
 }
-
-export default App;
